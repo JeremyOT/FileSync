df324f53c85fa395083a8c5b62f762f9c5f59362
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-05-16 01:00:41 -0700

    adding preference menu

---
+++ FileSync/FSPreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  FSPreferencesWindowController.h
+//  FileSync
+//
+//  Created by Jeremy Olmsted-Thompson on 5/16/12.
+//  Copyright (c) 2012 JOT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FSPreferencesWindowController : NSWindowController
+
+@end
---
+++ FileSync/FSPreferencesWindowController.m
@@ -0,0 +1,34 @@
+//
+//  FSPreferencesWindowController.m
+//  FileSync
+//
+//  Created by Jeremy Olmsted-Thompson on 5/16/12.
+//  Copyright (c) 2012 JOT. All rights reserved.
+//
+
+#import "FSPreferencesWindowController.h"
+
+@interface FSPreferencesWindowController ()
+
+@end
+
+@implementation FSPreferencesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
